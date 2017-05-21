a11d2cac23a7a8377b4104609a2c8b5cbbb77118
leonardpieper <leonard.pieper@outlook.com>
2017-05-21 17:50:04 +0200

    UI Update

--- src/main/java/com/github/leonardpieper/lupo_mobile/MainActivity.java
+++ src/main/java/com/github/leonardpieper/lupo_mobile/MainActivity.java
@@ -183,6 +183,8 @@ public class MainActivity extends AppCompatActivity
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        
+
         return true;
     }
 
